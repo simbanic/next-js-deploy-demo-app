6a40977e2b021aedd9efac13457a56fb3dd857f7
simbanic <alpesh.savaliya@gmail.com>
2026-01-05 11:58:14 +0000

    test ci

---
+++ infra/github-actions-policies.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_role_policy_attachment" "ecr" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
